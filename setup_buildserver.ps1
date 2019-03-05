605391d38dba6fdbf000fc36e2217018d4308fb7
bvanlierop <bartvanlierop@gmail.com>
2019-03-05 20:48:45 +0100

    Setup scripts for setting up build server

---
+++ setup_buildserver.ps1
@@ -0,0 +1,16 @@
+# General tooling
+choco install --confirm powershell --version 5.1.14409.20180811
+choco install --confirm rsat -params '"/SM"' --version 2.1809.0.20190205
+choco install --confirm notepadplusplus.install --version 7.6.3
+
+# Version Control System
+choco install --confirm git --version 2.21.0
+# TODO: setup SSH key
+
+# .NET Build Tooling
+choco install --confirm dotnet4.5.1 --version 4.5.20120822
+choco install --confirm dotnetcore-sdk --version 2.2.104
+#choco install --confirm visualstudio2017buildtools --version 15.9.7.0
+
+# CI server app
+#choco install --confirm jenkins --version 2.150.1
